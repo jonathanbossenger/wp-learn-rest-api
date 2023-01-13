591e710f1af01431b713598fe72d88816a9fb9b1
Jonathan Bossenger <jonathanbossenger@gmail.com>
2023-01-13 07:51:38 +0200

    Updating posts response

--- wp-learn-rest-api.js
+++ wp-learn-rest-api.js
@@ -44,10 +44,10 @@ const loadPostsButton = document.getElementById( 'wp-learn-rest-api-button' );
 if ( typeof ( loadPostsButton ) != 'undefined' && loadPostsButton != null ) {
     loadPostsButton.addEventListener( 'click', function () {
         const allPosts = new wp.api.collections.Posts();
-        allPosts.fetch().done( function () {
+        allPosts.fetch().done( function ( posts ) {
             const textarea = document.getElementById( 'wp-learn-posts' );
-            allPosts.models.forEach( function ( post ) {
-                textarea.value += post.get( 'title' ).rendered + '\n'
+            posts.forEach( function ( post ) {
+                textarea.value += post.title.rendered + '\n'
             } );
         } );
     } );
