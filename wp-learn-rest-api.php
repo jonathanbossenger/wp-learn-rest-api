a0b7a9282d9979bc2d731d2d2e2c892c1de32150
Jonathan Bossenger <jonathanbossenger@gmail.com>
2023-01-17 14:49:50 +0200

    Removing JQuery

--- wp-learn-rest-api.php
+++ wp-learn-rest-api.php
@@ -27,8 +27,7 @@ function wp_learn_rest_render_admin_page(){
 	?>
     <div class="wrap" id="wp_learn_admin">
         <h1>Admin</h1>
-        <button id="wp-learn-ajax-button">Load Posts via admin-ajax</button>
-        <button id="wp-learn-rest-api-button">Load Posts via REST</button>
+        <button id="wp-learn-rest-api-button">Load Posts</button>
         <button id="wp-learn-clear-posts">Clear Posts</button>
         <h2>Posts</h2>
         <textarea id="wp-learn-posts" cols="125" rows="15"></textarea>
@@ -44,31 +43,9 @@ function wp_learn_rest_enqueue_script() {
 	wp_register_script(
 		'wp-learn-rest-api',
 		plugin_dir_url( __FILE__ ) . 'wp-learn-rest-api.js',
-		array( 'jquery' ),
+		array( 'wp-api' ),
 		time(),
 		true
 	);
 	wp_enqueue_script( 'wp-learn-rest-api' );
-
-    /**
-     * Localize the script with the Ajax url to handle ajax requests
-     */
-	wp_localize_script(
-		'wp-learn-rest-api',
-		'wp_learn_ajax',
-		array(
-			'ajax_url' => admin_url( 'admin-ajax.php' ),
-		)
-	);
 }
-
-/**
- * Handles the learn_fetch_posts AJAX request.
- */
-add_action( 'wp_ajax_learn_fetch_posts', 'wp_learn_ajax_fetch_posts' );
-function wp_learn_ajax_fetch_posts() {
-	$posts = get_posts();
-    wp_send_json($posts);
-	wp_die(); // All ajax handlers die when finished
-}
-
