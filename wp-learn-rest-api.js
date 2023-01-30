9d314b5c5dcb41d9fb185cf34e1264491cffdc2e
Jonathan Bossenger <jonathanbossenger@gmail.com>
2023-01-30 16:47:14 +0200

    Bug fixes

--- wp-learn-rest-api.js
+++ wp-learn-rest-api.js
@@ -1,32 +1,4 @@
 /**
- * JQuery to handle the Ajax request
- */
-jQuery( document ).ready(
-    function ( $ ) {
-        const loadPostsButton = $( '#wp-learn-ajax-button' );
-        if ( typeof ( loadPostsButton ) != 'undefined' && loadPostsButton != null ) {
-            loadPostsButton.on(
-                'click',
-                function ( event ) {
-                    $.post(
-                        wp_learn_ajax.ajax_url,
-                        {
-                            'action': 'learn_fetch_posts',
-                        },
-                        function ( posts ) {
-                            const textarea = $( '#wp-learn-posts' );
-                            posts.forEach( function ( post ) {
-                                textarea.append( post.post_title + '\n' )
-                            } );
-                        },
-                    )
-                },
-            );
-        }
-    },
-);
-
-/**
  * Clear the text area button
  */
 const clearPostsButton = document.getElementById( 'wp-learn-clear-posts' );
--- wp-learn-rest-api.php
+++ wp-learn-rest-api.php
@@ -6,17 +6,18 @@
  */
 
 /**
- * Create an admin page to show the form submissions
+ * Create a Tools sub menu to show the admin form
  */
 add_action( 'admin_menu', 'wp_learn_rest_submenu', 11 );
 function wp_learn_rest_submenu() {
-	add_menu_page(
-		esc_html__( 'WP Learn Admin Page', 'wp_learn' ),
-		esc_html__( 'WP Learn Admin Page', 'wp_learn' ),
+	add_submenu_page(
+		'tools.php',
+		esc_html__( 'WP Learn REST API', 'wp_learn' ),
+		esc_html__( 'WP Learn REST API', 'wp_learn' ),
 		'manage_options',
-		'wp_learn_admin',
+		'wp-learn-rest-api',
 		'wp_learn_rest_render_admin_page',
-		'dashicons-admin-tools'
+		10
 	);
 }
 
@@ -27,7 +28,6 @@ function wp_learn_rest_render_admin_page(){
 	?>
     <div class="wrap" id="wp_learn_admin">
         <h1>Admin</h1>
-        <button id="wp-learn-ajax-button">Load Posts via admin-ajax</button>
         <button id="wp-learn-rest-api-button">Load Posts via REST</button>
         <button id="wp-learn-clear-posts">Clear Posts</button>
         <h2>Posts</h2>
@@ -44,31 +44,10 @@ function wp_learn_rest_enqueue_script() {
 	wp_register_script(
 		'wp-learn-rest-api',
 		plugin_dir_url( __FILE__ ) . 'wp-learn-rest-api.js',
-		array( 'jquery' ),
+		array( 'wp-api' ),
 		time(),
 		true
 	);
 	wp_enqueue_script( 'wp-learn-rest-api' );
-
-    /**
-     * Localize the script with the Ajax url to handle ajax requests
-     */
-	wp_localize_script(
-		'wp-learn-rest-api',
-		'wp_learn_ajax',
-		array(
-			'ajax_url' => admin_url( 'admin-ajax.php' ),
-		)
-	);
-}
-
-/**
- * Handles the learn_fetch_posts AJAX request.
- */
-add_action( 'wp_ajax_learn_fetch_posts', 'wp_learn_ajax_fetch_posts' );
-function wp_learn_ajax_fetch_posts() {
-	$posts = get_posts();
-    wp_send_json($posts);
-	wp_die(); // All ajax handlers die when finished
 }
 
